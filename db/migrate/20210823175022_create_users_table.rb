229d373e54bf3595cc5298ff576d6ad70f0adfdd
Erin <errose10@gmail.com>
2021-08-23 14:32:43 -0400

    add timestamp to users table migration

--- db/migrate/20210823175022_create_users_table.rb
+++ db/migrate/20210823175022_create_users_table.rb
@@ -1,4 +1,10 @@
 class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :password_digest
+      t.timestamps null: false
+    end
   end
 end
