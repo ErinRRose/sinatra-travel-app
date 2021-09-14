e5cfbff11b7e2ecbaa4c89df9baec3db61a665fe
Erin <errose10@gmail.com>
2021-09-14 15:45:37 -0400

    save last update,changed root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,41 +9,40 @@ class UsersController <ApplicationController
         #find the user
         @user = User.find_by(username: params[:username])
         #auth the user
-        if @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
          #log the user in  - create user session
          session[:user_id] = @user.id
          #redirect to the user's landing page
+
+         flash[:message] = "Welcome, #{@user.name}!"
          redirect "users/#{@user.id}"
         else
             #invalid, redirect
+            flash[:message] = "Your credentials are invalid  Please sign up or try again."
+            redirect '/login'
         end
-        
-        
-
-        
     end
-
-    
 
     get '/signup' do
         erb :signup
     end
 
     post '/users' do
-        if params[:name] != "" && params[:username] != "" && params[:password] != "" 
-            @user = User.create[params]
-            session[:user_id] = @user.id
-            redirect "/users/#{@user.id}"
-        else
-            redirect '/signup'
-            #include a message, letting them know what happened
-        end
-
+        #if params[:name] != "" && params[:username] != "" && params[:password] != "" 
+        @user = User.new(params)
+            if @user.save
+                session[:user_id] = @user.id
+                flash[:message] = "You have created a new account, #{@user.name}! Welcome!"
+                redirect "/users/#{@user.id}"
+            else
+                flash[:message] = "Account creation failure: #{@user.errors.full_messages.to_sentence}"
+                redirect '/signup'
+            end
     end
 
     #route for signup
 
-    get '/users/:id'
+    get '/users/:id' do
     #"user show route"
         @user = User.find_by(id: params[:id])
         erb :'/users/show' 
