285d4e67183ce2a5bf8f8564aa01d5fb0eddfa26
Erin <errose10@gmail.com>
2021-09-22 20:49:10 -0400

    added patach for edit option and only user edits

--- app/controllers/travel_entries_controller.rb
+++ app/controllers/travel_entries_controller.rb
@@ -20,14 +20,42 @@ class TravelEntriesController < ApplicationController
     end
     # show route for a travel entry
     get '/travel_entries/:id' do
-      @travel_entry = TravelEntry.find(params[:id])
-      erb :'travel_entries/show'
+      set_travel_entry
+      erb :'/travel_entries/show'
     end
 
     get '/travel_entries/:id/edit' do
-      @travel_entry = TravelEntry.find(params[:id])
-      erb :'/travel_entries/edit'
+      set_travel_entry
+      if logged_in?
+        if @travel_entry.user == current_user
+          erb :'/travel_entries/edit'
+        else
+          redirect "users/#{current_user.id}"
+        end
+      else
+        redirect '/'
+      end
+    end
+
+    patch '/travel_entries/:id' do
+      set_travel_entry
+      if logged_in?
+        if @travel_entry.user == current_user #&& params[:content] != ""
+          @travel_entry.update(content: params[:content], name_location: params[:name_location])
+          redirect "/travel_entries/#{@travel_entry.id}"
+        else
+          redirect "users/#{current_user.id}"
+        end
+      else
+        redirect '/'
+      end
     end
     # index route for all travel entries
+    private
+
+    def set_travel_entry
+      @travel_entry = TravelEntry.find(params[:id])
+    end
+
 
 end
